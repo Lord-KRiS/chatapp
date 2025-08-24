f85fe0d1d8a3b2ca92b8616d85f039b31ea8d386
KRRISH AGARWAL <krrishapj@gmail.com>
2025-08-24 23:22:10 +0530

    comments removed

--- client/src/utility/atoms.ts
+++ client/src/utility/atoms.ts
@@ -37,32 +37,3 @@ export const getClassesAtom = (room: string) =>
       `mr-auto bg-${color}-800 text-white text-left ml-2`,
     ];
   });
-
-// export const colorForRooms = new Map();
-// colorForRooms.set("public", "orange");
-
-// export const addColorToMap = (room: string, array: arrayType[]) => {
-//   const color = colors[array.length % colors.length];
-//   colorForRooms.set(room, color);
-// };
-
-// export const getColor = (array: arrayType[], room: string) => {
-//   console.log(colorForRooms);
-//   //   const idx = array.map((xx) => xx.room).findIndex((room) => room === curRoom);
-//   return colorForRooms.get(room);
-//   //   return colors[idx % colors.length];
-// };
-
-// export const getClasses = (array: arrayType[], room: string) => {
-//   //   const idx = array.map((xx) => xx.room).findIndex((room) => room === curRoom);
-
-//   //   console.log(colors[idx % colors.length]);
-
-//   const color = colorForRooms.get(room);
-//   //   const color = colors[idx % colors.length];
-
-//   return [
-//     `ml-auto bg-${color}-200 text-black text-right mr-2`,
-//     `mr-auto bg-${color}-800 text-white text-left ml-2`,
-//   ];
-// };
